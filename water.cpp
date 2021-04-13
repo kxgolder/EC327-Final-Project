04f25d3996e1eb00b3fb94a392438a1465b07288
Cole Hunter <colejh@bu.edu>
2021-04-13 17:53:01 -0400

    Background, water bar

--- water.cpp
+++ water.cpp
@@ -9,50 +9,93 @@ using std::string;
 using std::stoi;
 using std::vector;
 
-
-
+const int app_width = 1400;
+const int app_length = 700;
+const int calendar_spaces = 8;
+const int calendar_days = 7;
+const float rec_x = (float)350/calendar_spaces;
+const int rec_y = 40;
+const int rec_width = 150;
+const int rec_length = 300;
 
 int main(){
 
-sf::RenderWindow window(sf::VideoMode(1400, 700), "Calendar");
+sf::RenderWindow window(sf::VideoMode(app_width, app_length), "Calendar");
+
+
+// Textures 
+sf::Texture wood_background; // Background for weekdays
+if (!wood_background.loadFromFile("wood.jpg")){
+  cout << "didnt work\n";
+};
+wood_background.setSmooth(true);
+
+
+sf::Texture app_background; // Background for app
+if (!app_background.loadFromFile("pastel.jpg")){
+  cout << "didnt work\n";
+};
+app_background.setSmooth(true);
+
+sf::Texture water_texture; // water progress bar
+if (!water_texture.loadFromFile("water.jpg",sf::IntRect(0,120,480,240))){
+  cout << "didnt work\n";
+};
+app_background.setSmooth(true);
+
+// Background Shape
+sf::RectangleShape background;
+background.setSize(sf::Vector2f(app_width,app_length));
+background.setTexture(&app_background);
+
+// water progress bar
+sf::RectangleShape water_bar;
+water_bar.setSize(sf::Vector2f(app_width-2*rec_x,50));
+water_bar.setPosition(rec_x,rec_length+rec_y+40);
+water_bar.setTexture(&water_texture);
+
 
 // For the calendar shapes
 vector<sf::RectangleShape> rect_vec;
 sf::RectangleShape rectangle;
-rectangle.setSize(sf::Vector2f(200, 697));
+rectangle.setSize(sf::Vector2f(rec_width, rec_length));
 rectangle.setFillColor(sf::Color::Red);
 rectangle.setOutlineThickness(1);
-rectangle.setPosition(2,2);
+rectangle.setPosition(rec_x,rec_y);
+rectangle.setTexture(&wood_background);
 
 // First day to copy 
-sf::Text Sunday;
+int day_x = rec_x+50;
+int day_y = 50;
+sf::Text Sun;
 sf::Font font;
 font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
-Sunday.setFont(font);
-Sunday.setString("Sunday");
-Sunday.setFillColor(sf::Color::Black);
-Sunday.setOutlineColor(sf::Color::White);
-Sunday.setOutlineThickness(1);
-Sunday.setPosition(12,2);
+Sun.setFont(font);
+Sun.setString("Sun");
+Sun.setFillColor(sf::Color::Black);
+Sun.setOutlineColor(sf::Color::White);
+Sun.setOutlineThickness(1);
+Sun.setPosition(day_x,day_y);
 
 // Names for the Calendar boxes
-vector<string> days = {"Monday",
-"Tuesday","Wednesday","Thursday","Friday","Saturday"};
+vector<string> days = {"Mon",
+"Tue","Wed","Thu","Fri","Sat"};
 vector<sf::Text> day;
-int x = 200;
+
+int x = day_x+50; // spacing on these needs to be calculated
 for (int i = 0; i<days.size();i++){
-  day.push_back(Sunday);
-  day.at(i).setPosition(12+x,2);
+  day.push_back(Sun);
+  day.at(i).setPosition(150+x,day_y);
   day.at(i).setString(days.at(i));
-  x = x+200;
+  x = x+rec_x+rec_width;
 }
 
 //Creating the Day Boxes
-int y = 200;
+float a = rec_width+(float)350/calendar_spaces;
 for (int i = 0;i<6;i++){
   rect_vec.push_back(rectangle);
-  rect_vec.at(i).setPosition(2+y,2);
-  y= y + 200;
+  rect_vec.at(i).setPosition(rec_x+a,rec_y);
+  a= a+rec_width+(float)350/calendar_spaces;
 }
 
 
@@ -61,6 +104,9 @@ for (int i = 0;i<6;i++){
 window.setFramerateLimit(60);
 while(window.isOpen()){
 
+// background image
+window.draw(background);
+window.draw(water_bar); // need to link this to the water input, cover up 16-remaining for a progress bar
 
 // Draw the boxes
 window.draw(rectangle);
@@ -68,9 +114,10 @@ for (int i = 0; i < rect_vec.size();i++)
   window.draw(rect_vec.at(i));
 
 // Draw the names
-window.draw(Sunday);
+window.draw(Sun);
 for (int i = 0; i < day.size();i++)
   window.draw(day.at(i));
+
 
 window.display();
 
