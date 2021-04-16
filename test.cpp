04d15ada676d7713e2b2747e1fd0e549761e5032
Katharina Golder <kxgolder@bu.edu>
2021-04-16 00:17:59 -0400

    new test.cpp

--- test.cpp
+++ test.cpp
@@ -2,43 +2,70 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <chrono>
-
 #include "Textbox.h"
+#include "Button.h"
 
 int main() {
   sf::RenderWindow window;
 
-  sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 445,
-    (sf::VideoMode::getDesktopMode().height / 2) - 480);
+  sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 445, (sf::VideoMode::getDesktopMode().height / 2) - 480);
 
-
-  window.create(sf::VideoMode(900, 900), "SFML Project", sf::Style::Titlebar | sf::Style::Close);
+  window.create(sf::VideoMode(900, 900), "SFML Textbox", sf::Style::Titlebar | sf::Style::Close);
   window.setPosition(centerWindow);
 
   window.setKeyRepeatEnabled(true);
 
   sf::Font font;
-  font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+  if (!font.loadFromFile("arial.ttf"))
+    std::cout << "Font not found!\n";
 
-  Textbox textbox1(15, sf::Color::White, false);
-  textbox1.setFont(font);
-  textbox1.setPosition({100,100});
+  Textbox text1(20, sf::Color::White, true);
+  text1.setPosition({ 100, 100 });
+  text1.setLimit(true, 30);
+  text1.setFont(font);
 
+  Button btn1("Enter", { 200, 100 }, 30, sf::Color::Green, sf::Color::Black);
+  btn1.setFont(font);
+  btn1.setPosition({ 100, 300 });
   
-  while(window.isOpen()){
+
+  //Main Loop:
+  while (window.isOpen()) {
 
     sf::Event Event;
 
-    while(window.pollEvent(Event)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+      text1.setSelected(true);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+      text1.setSelected(false);
+    }
+
+    //Event Loop:
+    while (window.pollEvent(Event)) {
       switch (Event.type) {
 
       case sf::Event::Closed:
         window.close();
+      case sf::Event::TextEntered:
+        text1.typedOn(Event);
+      case sf::Event::MouseMoved:
+        if (btn1.isMouseOver(window)) {
+          btn1.setBackColor(sf::Color::Magenta);
+        }
+        else {
+          btn1.setBackColor(sf::Color::Green);
+        }
+        break;
+      case sf::Event::MouseButtonPressed:
+        if (btn1.isMouseOver(window)) {
+          std::cout << "Hello " << text1.getText() << "\n";
+        }
       }
     }
     window.clear();
-    textbox1.drawTo(window);
+    text1.drawTo(window);
+    btn1.drawTo(window);
     window.display();
   }
 }
